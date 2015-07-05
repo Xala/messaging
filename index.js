25fdb005269b5af148b2f2acd2c540875f0ddebd
Jacob Greenberg <jacobgre@kth.se>
2015-07-05 19:18:15 +0200

    dynamic timestamp
    Takes google first name, dynamic timestamp, "send as $user" no name box.

--- index.js
+++ index.js
@@ -26,6 +26,7 @@
 
     // store the active form shown on the page
     var activeForm = null;
+    var googleUser = getStoredValue('googleUser');
 
     var alertBox = $('#alert');
 
@@ -45,6 +46,8 @@
 
             if (user) {
                 deferred.resolve(user);
+                googleUser = user.google.displayName;
+                storeValue('googleUser', googleUser);
             }
         });
 
@@ -139,35 +142,40 @@
         userRef = rootRef.child('users').child(user.uid);
         userRef.once('value', function (snap) {
             var user = snap.val();
+            $('<p/>').text('Send as ' + getFirstNameFromGoogle(googleUser)).appendTo($('#userThatSendMessage'));;
             if (!user) {
                 return;
             }
 
             // set name field
             form.find('#nameInput').val(user.name);
+            window.alert(user.name);
+            googleUser = user.name;
         });
 
         // Save user's info to Firebase
         form.on('submit', function (e) {
             e.preventDefault();
 		    var name = $('#nameInput').val();
+            var name = getFirstNameFromGoogle(googleUser);
 			var text = $('#messageInput').val();
 			var d = new Date();
-			var curr_date = d.getDate();
-			var curr_month = d.getMonth() + 1; //Months are zero based
-			var curr_year = d.getFullYear();
-			var time = curr_date + "-" + curr_month + "-" + curr_year;
+			var time = Date.now()/1000;
 			rootRef.push({name: name, text: text, time: time});
 			$('#messageInput').val('');
         });
-        rootRef.on('child_added', function(snapshot) {
+        /*rootRef.on('child_added', function(snapshot) {
         	var message = snapshot.val();
-        	//displayChatMessage(message.name, message.text, message.time);
+        	displayChatMessage(message.name, message.text, message.time);
       	});
       	function displayChatMessage(name, text, time) {
         	$('<div/>').text(text).prepend($('<em/>').text(name+'('+time+'): ')).appendTo($('#messagesDiv'));
         	$('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-      	};
+      	};*/
+        function getFirstNameFromGoogle(name) 
+        {
+            return name.split(' ')[0];
+        };
 
     };
 
@@ -257,9 +265,87 @@
 
     });
 
+    rootRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text, message.time);
+    });
+    function displayChatMessage(name, text, time) {
+        //$('<div/>').text(text).prepend($('<em/>').text(name+'('+time+'): ')).appendTo($('#messagesDiv'));
+        $('<div/>').text(text).prepend('<em/>' + name + ' (' + '<span data-livestamp=\"' + time + '\">just now</span>' + '): ').appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+    };
+//<span data-livestamp="1436113177"></span>.
+
+
+
+
+    function storeValue(key, value) {
+        if (localStorage) {
+            localStorage.setItem(key, value);
+        } else {
+            $.cookies.set(key, value);
+        }
+    }
+
+    function getStoredValue(key) {
+        if (localStorage) {
+            return localStorage.getItem(key);
+        } else {
+            return $.cookies.get(key);
+        }
+    }
+
+    /*function time_ago(time){
+
+        switch (typeof time) {
+            case 'number': break;
+            case 'string': time = +new Date(time); break;
+            case 'object': if (time.constructor === Date) time = time.getTime(); break;
+            default: time = +new Date();
+        }
+        var time_formats = [
+            [60, 'seconds', 1], // 60
+            [120, '1 minute ago', '1 minute from now'], // 60*2
+            [3600, 'minutes', 60], // 60*60, 60
+            [7200, '1 hour ago', '1 hour from now'], // 60*60*2
+            [86400, 'hours', 3600], // 60*60*24, 60*60
+            [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
+            [604800, 'days', 86400], // 60*60*24*7, 60*60*24
+            [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+            [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
+            [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+            [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+            [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+            [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+            [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+            [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+        ];
+        var seconds = (+new Date() - time) / 1000,
+            token = 'ago', list_choice = 1;
+
+        if (seconds == 0) {
+            return 'Just now'
+        }
+        if (seconds < 0) {
+            seconds = Math.abs(seconds);
+            token = 'from now';
+            list_choice = 2;
+        }
+        var i = 0, format;
+        while (format = time_formats[i++]){
+            if (seconds < format[0]) {
+                if (typeof format[2] == 'string')
+                    return format[list_choice];
+                else
+                    return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+            }
+        }
+        return time;
+    }*/
+
 }(window.jQuery, window.Firebase, window.Path))
 
-var myDataRef = new Firebase('https://popping-inferno-1335.firebaseio.com/');
+/*var myDataRef = new Firebase('https://popping-inferno-1335.firebaseio.com/');
       $('#messageInput').keypress(function (e) {
         if (e.keyCode == 13) {
           mydataRef.authWithOAuthPopup("google", function(error, authData) {
@@ -286,4 +372,4 @@ var myDataRef = new Firebase('https://popping-inferno-1335.firebaseio.com/');
       function displayChatMessage(name, text, time) {
         $('<div/>').text(text).prepend($('<em/>').text(name+'('+time+'): ')).appendTo($('#messagesDiv'));
         $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-      };
+      };*/
